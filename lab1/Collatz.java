4b8edd4d4c056e0500c126f25ca156986336b253
Yun Zhu <yunzhu0717@gmail.com>
2024-01-10 17:14:17 +0000

    Added Lab 4 Starter Files

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,16 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author Yun Zhu
+ *  @author YOUR NAME HERE
  */
 public class Collatz {
 
-    /** Return the next number */
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 1) {
-            return n;
-        } else if (n % 2 != 0) {
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
             return 3 * n + 1;
         } else {
-            return n / 2;
+            return n * 2;
         }
     }
 
