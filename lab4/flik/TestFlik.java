ddfeb69bd27831bee6fec84af053cd441a5b88d0
Yun Zhu <yunzhu0717@gmail.com>
2024-01-10 16:02:34 +0000

    complete lab4

---
+++ lab4/flik/TestFlik.java
@@ -0,0 +1,12 @@
+package flik;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestFlik {
+    @Test
+    public void TestFlik() {
+        assertTrue(Flik.isSameNumber(128, 128));
+    }
+}
